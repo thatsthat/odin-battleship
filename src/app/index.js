c5dd043f1f10fb9febad87dea82b78a3a390b6ce
Aram <aram.anto@gmail.com>
2022-11-09 16:47:20 +0100

    implemented insert ship function

--- src/app/index.js
+++ src/app/index.js
@@ -13,6 +13,54 @@ const Ship = (shipLength, numHits = 0, sunk = false) => {
   return { shipLength, showHits, sunk, hit };
 };
 
-//const taskList = (() => {})();
+const Gameboard = () => {
+  // Ship types: 1-Carrier, 2-Battleship, 3-Destroyer, 4-Submarine
+  // 5-Patrol Boat
+  const shipSizes = [5, 4, 3, 3, 2];
+  // Create a 10 x 10 board
+  const board = new Array(10);
+  board.fill(new Array(10));
+  // Create an array to store all ships on the board
+  const ships = [];
+  const placeShip = (shipType, startCoords, orientation) => {
+    if (shipType < 1 || shipType > 5) {
+      throw "Invalid ship type";
+    }
+    const shipSize = shipSizes[shipType - 1];
+    const outsideBoard = (v) => {
+      v < 0 || v >= 10;
+    };
+    if (startCoords.some(outsideBoard)) {
+      throw "Invalid start coordinates";
+    }
+    if (orientation == "h") {
+      const endCoords = [startCoords[0] + shipSize, startCoords[1]];
+      // Check that the ship is placed within the board limits
+      if (endCoords.some(outsideBoard)) {
+        throw "The ship must be place within the board limits";
+      } else {
+        ships.push(new Ship(shipSizes[shipType]));
+        for (let i = 0; i < shipSize; i++) {
+          board[startCoords[1]][startCoords[0] + i] = ships.length;
+        }
+      }
+    } else if (orientation == "v") {
+      const endCoords = [startCoords[0], startCoords[1] + shipSize];
+      // Check that the ship is placed within the board limits
+      if (endCoords.some(outsideBoard)) {
+        throw "The ship must be place within the board limits";
+      } else {
+        ships.push(new Ship(shipSizes[shipType]));
+        for (let i = 0; i < shipSize; i++) {
+          board[startCoords[1] + i][startCoords[0]] = ships.length;
+        }
+      }
+    }
+  };
+  const getBoard = () => {
+    return board;
+  };
+  return { getBoard, placeShip };
+};
 
-export { Ship };
+export { Ship, Gameboard };
--- src/app/index.test.js
+++ src/app/index.test.js
@@ -12,3 +12,24 @@ test("Hit a ship", () => {
   testShip2.hit();
   expect(testShip2.showHits()).toBe(1);
 });
+
+test("Create an empty Gameboard", () => {
+  const eB = new Array(10);
+  eB.fill(new Array(10));
+  const testBoard = tF.Gameboard();
+  console.log(testBoard);
+  expect(testBoard.getBoard()).toEqual(eB);
+});
+
+test("Place a Carrier ship on the board", () => {
+  const eB = new Array(10);
+  eB.fill(new Array(10));
+  eB[0][0] = 1;
+  for (let i = 0; i < 5; i++) {
+    //eB[0][i] = 1;
+  }
+  console.log(eB);
+  const testBoard = tF.Gameboard();
+  testBoard.placeShip(1, [0, 0], "v");
+  expect(testBoard.getBoard()).toEqual(eB);
+});
