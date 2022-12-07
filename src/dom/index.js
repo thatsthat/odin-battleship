307c9be584cfd7a0098eb2c8f75329f1478ef597
Aram <aram.anto@gmail.com>
2022-12-07 17:36:48 +0100

    moved html playground code into webpack

--- src/dom/index.js
+++ src/dom/index.js
@@ -2,11 +2,35 @@ import "./style.css";
 //import "@mdi/font/css/materialdesignicons.css";
 import { taskList } from "../app/index.js";
 
+const plainHTML = `
+  <div id="content">
+    <div id="boards">
+      <div id="player1"></div>
+      <div id="player2"></div>
+    </div>
+  </div>
+  <script src="index.js"></script>`;
+
 function renderUI() {
-  const hidude = document.createElement("div");
-  hidude.textContent = "Dude where is my party now";
-  const cont = document.getElementById("content");
-  cont.appendChild(hidude);
+  document.body.innerHTML = plainHTML;
+
+  const gridP1 = document.getElementById("player1");
+
+  for (let i = 0; i < 100; i++) {
+    let cell = document.createElement("div");
+    cell.id = `a${i}`;
+    cell.textContent = "0";
+    gridP1.appendChild(cell);
+  }
+
+  const gridP2 = document.getElementById("player2");
+
+  for (let i = 0; i < 100; i++) {
+    let cell = document.createElement("div");
+    cell.id = `b${i}`;
+    cell.textContent = "1";
+    gridP2.appendChild(cell);
+  }
 }
 
 export { renderUI };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { app } from "./app/index.js";
 import { renderUI } from "./dom/index.js";
 
-const content = document.createElement("div");
-content.id = "content";
-document.body.append(content);
 renderUI();
